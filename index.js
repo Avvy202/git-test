8317dccb9a9cb5ab5bf21fabdb1b3229eb0bc95f
Avvy202 <vvanh201299@gmail.com>
2021-04-16 21:00:54 +0700

    add realtime clock

--- index.js
+++ index.js
@@ -20,3 +20,23 @@ btnTimes.addEventListener('click', function(){
         navBar.classList.remove("show-nav") 
     }
 })
+
+function realtimeCLock() {
+    var rtClock = new Date();
+
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
+
+    var amPm = (hours < 12) ? "AM" : "PM";
+
+    hours = (hours > 12) ? hours - 12: hours;
+
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    document.getElementById("clock").innerHTML = hours + " : " + minutes + " : " +seconds + " " + amPm;
+    var t = setTimeout(realtimeCLock, 500);
+}
+realtimeCLock();
